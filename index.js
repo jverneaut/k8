811d56f725ccab838b755ebfd6d2c26dd1b30620
Julien Verneaut <jverneaut@gmail.com>
2020-03-20 14:55:47 +0100

    Test

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 const express = require('express');
 const path = require('path');
 const redis = require('redis');
-// const client = redis.createClient();
+
+const redisURL = process.env.REDIS_URL || 'redis://localhost:6379';
+
+const connectDb = async () => {
+  let success = false;
+  let client;
+
+  while (!success) {
+    try {
+      client = redis.createClient(redisURL);
+      success = true;
+    } catch {
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+  }
+
+  return client;
+};
 
 const init = async () => {
+  const db = await connectDb();
+
   const app = express();
   app.use(express.static(path.join(__dirname, '/public')));
 
-  // client.incr('pod_index');
-  // const podIndex = await new Promise(resolve => {
-  //   client.get('pod_index', (err, reply) => {
-  //     resolve(reply);
-  //   });
-  // });
+  db.incr('pod_index');
+  const podIndex = await new Promise(resolve => {
+    db.get('pod_index', (err, reply) => {
+      resolve(reply);
+    });
+  });
 
   app.get('/api', async (req, res) => {
     res.json({
       hello: 'World',
       time: Date.now(),
-      // podIndex,
+      podIndex,
     });
   });
 
