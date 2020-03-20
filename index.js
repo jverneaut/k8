e275ad93e79175214b8155d5d47e300d08860de0
Julien Verneaut <jverneaut@gmail.com>
2020-03-20 12:16:24 +0100

    Take some time to restart

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
-app.use(express.static(path.join(__dirname, '/public')));
-
-const random = Math.random();
-
-app.get('/api', (req, res) => {
-  res.json({
-    hello: 'World',
-    random,
-    time: Date.now(),
-  });
-});
-
 setTimeout(() => {
-  process.exit(1);
-}, Math.random() * 30000 + 30000);
+  const app = express();
+  app.use(express.static(path.join(__dirname, '/public')));
 
-const port = 3000;
-app.listen(port, () => console.log(`App listening on port ${port}`));
+  const random = Math.random();
+
+  app.get('/api', (req, res) => {
+    res.json({
+      hello: 'World',
+      random,
+      time: Date.now(),
+    });
+  });
+
+  setTimeout(() => {
+    process.exit(1);
+  }, Math.random() * 30000 + 30000);
+
+  const port = 3000;
+  app.listen(port, () => console.log(`App listening on port ${port}`));
+}, 10000);
