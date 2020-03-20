8a7025c2d6c512097e8c337b47888fdecef15455
Julien Verneaut <jverneaut@gmail.com>
2020-03-20 12:19:16 +0100

    Test

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,6 @@ RUN npm install
 COPY . .
 EXPOSE 3000
 
+RUN sleep 10;
+
 CMD ["npm", "run", "start"]
--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const express = require('express');
 const path = require('path');
 
-setTimeout(() => {
-  const app = express();
-  app.use(express.static(path.join(__dirname, '/public')));
+const app = express();
+app.use(express.static(path.join(__dirname, '/public')));
 
-  const random = Math.random();
+const random = Math.random();
 
-  app.get('/api', (req, res) => {
-    res.json({
-      hello: 'World',
-      random,
-      time: Date.now(),
-    });
+app.get('/api', (req, res) => {
+  res.json({
+    hello: 'World',
+    random,
+    time: Date.now(),
   });
+});
 
-  setTimeout(() => {
-    process.exit(1);
-  }, Math.random() * 30000 + 30000);
+setTimeout(() => {
+  process.exit(1);
+}, Math.random() * 30000 + 30000);
 
-  const port = 3000;
-  app.listen(port, () => console.log(`App listening on port ${port}`));
-}, 10000);
+const port = 3000;
+app.listen(port, () => console.log(`App listening on port ${port}`));
