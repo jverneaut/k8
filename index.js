e0ea809572d831ed2cce0174af6f4f4b02b99889
Julien Verneaut <jverneaut@gmail.com>
2020-03-20 14:27:16 +0100

    Add redis

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const path = require('path');
+const redis = require('redis');
+const client = redis.createClient();
 
-const app = express();
-app.use(express.static(path.join(__dirname, '/public')));
+const init = async () => {
+  const app = express();
+  app.use(express.static(path.join(__dirname, '/public')));
 
-const random = Math.random();
-
-app.get('/api', (req, res) => {
-  res.json({
-    hello: 'World',
-    random,
-    time: Date.now(),
+  client.incr('pod_index');
+  const podIndex = await new Promise(resolve => {
+    client.get('pod_index', (err, reply) => {
+      resolve(reply);
+    });
   });
-});
 
-const port = 3000;
-app.listen(port, () => console.log(`App listening on port ${port}`));
+  app.get('/api', async (req, res) => {
+    res.json({
+      hello: 'World',
+      time: Date.now(),
+      podIndex,
+    });
+  });
+
+  const port = 3000;
+  app.listen(port, () => console.log(`App listening on port ${port}`));
+};
+
+init();
