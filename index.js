a36c8f2871d4c22e63e91ae3b8e4c01f59026f03
Julien Verneaut <jverneaut@gmail.com>
2020-03-20 14:48:34 +0100

    Oopsi

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const redis = require('redis');
-const client = redis.createClient();
+// const client = redis.createClient();
 
 const init = async () => {
   const app = express();
