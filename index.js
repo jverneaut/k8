2064a50d20517782e63565d8ac61123558bf4c75
Julien Verneaut <jverneaut@gmail.com>
2020-03-20 15:09:21 +0100

    Test

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const init = async () => {
   });
 
   const port = 3000;
-  app.listen(port, () => console.log(`App listening on port ${port}`));
+  server.listen(port, () => console.log(`App listening on port ${port}`));
 };
 
 init();
